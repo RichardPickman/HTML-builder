b252eac196c6cda297c7d9c2cecd2c5a883c0ad5
RichardPickman <drezzerock@gmail.com>
2022-05-19 17:45:34 +0200

    feat: solve task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+const { stat } = require('fs');
+const { readdir } = require('fs/promises');
+const { resolve, extname } = require('path');
+
+const currentPath = resolve(__dirname + '/', 'secret-folder')
+
+async function getFolderFiles(folder = currentPath) {
+    const folderName = folder;
+    const dirFiles = await readdir(folderName, { withFileTypes: true });
+    
+    dirFiles.forEach(file => {
+        if (file.isDirectory()) {
+            getFolderFiles(currentPath + '/' + file.name);
+        } else {
+            const pathName = resolve(folderName + '/', file.name);
+            
+            stat(pathName, (err, stats) => {
+                if (err) console.log(err);
+
+                const fileInfo = file.name.split('.');
+                console.log(`${fileInfo[0] || 'Doesn\'t have name'} - ${fileInfo[1]} - ${stats.size}`);
+
+            })
+        }
+    })
+}
+
+getFolderFiles();
