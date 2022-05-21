92979c6363dd4bb0c5f73657e97b38fa571a6bdb
RichardPickman <drezzerock@gmail.com>
2022-05-21 18:39:06 +0200

    feat: solve task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const { readdir, mkdir, copyFile } = require('fs/promises');
+const { getAllFiles } = require('../00-helper');
+const { join } = require('path');
 
+async function reusableCopyDir(checkDir, dirName) {
+    const filesArr = await readdir(checkDir, { withFileTypes: true });
+
+    mkdir(dirName, { recursive: true });
+
+    filesArr.forEach(file => {
+        const initialFolder = join(checkDir, file.name);
+        const outputFolder = join(dirName, file.name);
+        
+        if (file.isDirectory()) reusableCopyDir(initialFolder, outputFolder);
+        else copyFile(initialFolder, outputFolder);
+    })
+}
+
+module.exports = reusableCopyDir;
